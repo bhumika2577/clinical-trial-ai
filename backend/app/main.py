53f8f91eaf039e123b7d050e4f9f9649fdb23145
Bhumika Akula <you@example.com>
2026-02-06 02:36:15 +0530

    Remove create_all to prevent DB connection on startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(patient.router, prefix="/patient")
